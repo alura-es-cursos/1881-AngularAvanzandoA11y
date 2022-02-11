7cff58fc610d6a0bf5372951f5ac08414f9e380d
Harland Lohora <harland@lohora.com>
2022-02-11 13:55:42 -0300

    clase03 completada

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { AppComponent } from './app.component';
 import { ModalModule } from './shared/components/modal/modal.module';
 import { ComponentLoaderDirective } from './shared/directives/componentLoader/component-loader.directive';
 import { PostsModule } from './shared/components/posts/posts.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent, ComponentLoaderDirective],
-  imports: [BrowserModule, ModalModule, PostsModule],
+  imports: [BrowserModule, ModalModule, PostsModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/shared/components/modal/modal.component.ts
+++ src/app/shared/components/modal/modal.component.ts
@@ -1,12 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostBinding, OnInit } from '@angular/core';
 import { ModalConfig } from './modal-config';
+import { fade } from '../../animations/fade';
 
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
   styleUrls: ['./modal.component.scss'],
+  animations: [fade],
 })
 export class ModalComponent implements OnInit {
+  @HostBinding('@fade') fade = true;
   constructor() {}
 
   ngOnInit(): void {}
